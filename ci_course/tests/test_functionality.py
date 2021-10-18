3278ede13dda7324621aff9975df2aba7988a8ab
Ian McFarlane <ian.mcfarlane@dtc.ox.ac.uk>
2021-10-18 15:12:58 +0100

    Add test for minimum() where nothing is returned

--- ci_course/tests/test_functionality.py
+++ ci_course/tests/test_functionality.py
@@ -13,7 +13,7 @@ class TestFunctionality(unittest.TestCase):
         self.assertEqual(ci_course.minimum(1, 2, 3), 1)
         self.assertEqual(ci_course.minimum(1.2, 2.3), 1.2)
         self.assertEqual(ci_course.minimum(-1.2, -3), -3)
-
+        self.assertEqual(ci_course.minimum('hello','horse'), None)
 
 if __name__ == '__main__':
     unittest.main()
