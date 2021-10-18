65127bebac83f22bf751f87f65f0753c2a40b7be
Ian McFarlane <ian.mcfarlane@dtc.ox.ac.uk>
2021-10-18 15:15:32 +0100

    Fix linting errors in minimum test

--- ci_course/tests/test_functionality.py
+++ ci_course/tests/test_functionality.py
@@ -13,7 +13,8 @@ class TestFunctionality(unittest.TestCase):
         self.assertEqual(ci_course.minimum(1, 2, 3), 1)
         self.assertEqual(ci_course.minimum(1.2, 2.3), 1.2)
         self.assertEqual(ci_course.minimum(-1.2, -3), -3)
-        self.assertEqual(ci_course.minimum('hello','horse'), None)
+        self.assertEqual(ci_course.minimum('hello', 'horse'), None)
+
 
 if __name__ == '__main__':
     unittest.main()
